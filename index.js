124e16ff0b8cf46f1d75f975650dcc57487523b3
gitHubMuskan <muskangarg2890@gmail.com>
2026-08-14 01:33:49 +0530

    read more set

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express= require('express');
 const app= express();
 const path= require('path');
+const fs= require('fs');
 
 app.set('view engine','ejs');
 app.use(express.json());
@@ -8,7 +9,25 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/',(req,res)=>{
-    res.render('index');
+    fs.readdir(`./files`,(err,files)=>{
+        if(err){
+            res.render('index',{files:[]});
+        } else {
+            res.render('index',{files:files});
+        }
+    });
+});
+
+app.post('/create',(req,res)=>{
+    fs.writeFile(`./files/${req.body.title.split(' ').join("-")}.txt`,req.body.details,(err)=>{
+        res.redirect('/');
+    });
+});
+
+app.get('/files/:filename',(req,res)=>{
+    fs.readFile(`./files/${req.params.filename}`,'utf-8',(err,data)=>{
+        res.render('file',{data:data, filename:req.params.filename});
+    });  
 });
 
 app.listen(3000,()=>{
